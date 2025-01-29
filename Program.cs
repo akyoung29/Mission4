d42748cec3da21b48a619a8bfe8d698805713923
annay <akyoung@byu.edu>
2025-01-29 12:38:10 -0700

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿// Driver class
+
+// Welcome the user to the game
+
+// Create a game board array to store the players' choices
+
+// Ask each player in turn for their choice and update the game board array
+
+// Print the board by calling the method in the supporting class
+
+// Check for a winner by calling the method in the supporting class
