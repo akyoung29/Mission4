3fbd3a7fe810633daf7d18e2b418af4bfb8f0ea2
sadie21393 <sadie21@byu.edu>
2025-01-29 13:50:25 -0700

    Here are my changes to the main program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,66 @@
 ﻿// Driver class
+// Initiate variables 
+using System.Runtime.InteropServices;
+using Mission4;
+
+TicTacTools tt = new TicTacTools();
+//game board array to store player's choices
+List<string> gameBoard = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; 
+
+string oneGuess = "";
+string twoGuess = "";
+
+bool winner = false;
+int turnCount = 0;
 
 // Welcome the user to the game
+Console.WriteLine("Welcome to group 3-1's TicTacToe Game! Good luck!");
+Console.WriteLine();
+//Get player information
 
-// Create a game board array to store the players' choices
+Console.WriteLine("Player 1 will be Xs");
 
-// Ask each player in turn for their choice and update the game board array
+Console.WriteLine($"Player 2 will be Os");
+Console.WriteLine();
 
-// Print the board by calling the method in the supporting class
+//display the first game board 
+tt.PrintBoard(gameBoard);
 
-// Check for a winner by calling the method in the supporting class
+//Game loop begins until winner 
+do
+{
+        //PLAYER 1
+        // Ask each player in turn for their choice 
+        Console.WriteLine($"{playerOne} please choose where you would like to play (1-9): ");
+        oneGuess = Console.ReadLine();
+        //validate guess
+        tt.ValidateGuess(oneGuess);
+        //update board array
+        gameBoard.Insert(int.Parse(oneGuess), "X");
+
+        tt.PrintBoard(gameBoard);
+
+        //PLAYER 2
+        // Ask each player in turn for their choice 
+        Console.WriteLine($"{playerTwo} please choose where you would like to play (1-9): ");
+        twoGuess = Console.ReadLine();
+        //validate guess
+        tt.ValidateGuess(twoGuess);
+        //update board array
+        gameBoard.Insert(int.Parse(twoGuess), "O");
+
+        //increment turncounter 
+        turnCount++;
+
+        tt.PrintBoard(gameBoard);
+        winner = tt.ReturnWinner(gameBoard);
+
+        if (turnCount >= 9 & winner == false)
+        {
+        winner = true; //not actually a winner it is a cat game
+        Console.WriteLine("GAME OVER! NO PLAYER WON THE GAME");
+        }
+
+    } while (winner == false);
+
+Console.WriteLine("Thanks for playing! To play again, exit and restart the program");
